5e88b20ca2120c845b124e1881c2dc9a0fb133f5
Alex Dumitru <dumitru.alex.python@gmail.com>
2019-03-14 07:02:39 +0100

    WIP - calculator moved several properties in a loop

--- sandbox/calculator.py
+++ sandbox/calculator.py
@@ -4,12 +4,12 @@ from tkinter import *
 
 FG_CLR_BTN = 'black'
 BG_CLR_BTN = 'white'
+BG_CLR_BTN_OPER = '#eee'
 WIDTH_BTN  = 10
 HEIGHT_BTN = 3
-BD_BTN = 0
-CURSOR_BTN = 'hand2'
+BD_BTN = 0 # ?
+CURSOR_BTN = 'hand2' # ?
 NUMPAD = [str(num) for num in range(10)]
-
 
 # creating basic window
 window = Tk()
@@ -35,7 +35,10 @@ def btn_equal():
     global expression
     # catch syntaxerror on eval - make action when pressing again the button
     # win calc remembers last operation and redo it
-    result = str(eval(expression)) # 'eval' function evalutes the string expression directly
+    try:
+        result = str(eval(expression)) # 'eval' function evalutes the string expression directly
+    except SyntaxError:
+        expression = ""
     # you can also implement your own function to evalute the expression istead of 'eval' function
     input_text.set(result)
     expression = ""
@@ -54,7 +57,12 @@ input_frame.pack(side = TOP)
 
 
 # creating a input field inside the 'Frame'
-input_field = Entry(input_frame, font = ('arial', 18, 'bold'), textvariable = input_text, width = 50, bg = "#eee", bd = 0, justify = RIGHT)
+input_field = Entry(input_frame, font = ('arial', 18, 'bold'), 
+                    textvariable = input_text, 
+                    width = 50, 
+                    bg = "#eee", 
+                    bd = 0, 
+                    justify = RIGHT)
 input_field.grid(row = 0, column = 0)
 input_field.pack(ipady = 10) # 'ipady' is internal padding to increase the height of input field
 
@@ -65,35 +73,35 @@ btns_frame.pack()
 
 
 # first row
-clear = Button(btns_frame, text = "C", fg = "black", width = 32, height = 3, bd = 0, bg = "#eee", cursor = "hand2", command = lambda: btn_clear()).grid(row = 0, column = 0, columnspan = 3, padx = 1, pady = 1)
-divide = Button(btns_frame, text = "/", fg = "black", width = 10, height = 3, bd = 0, bg = "#eee", cursor = "hand2", command = lambda: btn_click("/")).grid(row = 0, column = 3, padx = 1, pady = 1)
+clear = Button(btns_frame, text = "C", command = lambda: btn_clear()).grid(row = 0, column = 0, columnspan = 3, padx = 1, pady = 1)
+divide = Button(btns_frame, text = "/", command = lambda: btn_click("/")).grid(row = 0, column = 3, padx = 1, pady = 1)
 
 
 # second row
 seven    = Button(btns_frame, text = "7", command = lambda: btn_click(7)).grid(row = 1, column = 0, padx = 1, pady = 1)
 eight    = Button(btns_frame, text = "8", command = lambda: btn_click(8)).grid(row = 1, column = 1, padx = 1, pady = 1)
 nine     = Button(btns_frame, text = "9", command = lambda: btn_click(9)).grid(row = 1, column = 2, padx = 1, pady = 1)
-multiply = Button(btns_frame, text = "*", fg = "black", width = 10, height = 3, bd = 0, bg = "#eee", cursor = "hand2", command = lambda: btn_click("*")).grid(row = 1, column = 3, padx = 1, pady = 1)
+multiply = Button(btns_frame, text = "*", command = lambda: btn_click("*")).grid(row = 1, column = 3, padx = 1, pady = 1)
 
 
 # third row
 four  = Button(btns_frame, text = "4", command = lambda: btn_click(4)).grid(row = 2, column = 0, padx = 1, pady = 1)
 five  = Button(btns_frame, text = "5", command = lambda: btn_click(5)).grid(row = 2, column = 1, padx = 1, pady = 1)
 six   = Button(btns_frame, text = "6", command = lambda: btn_click(6)).grid(row = 2, column = 2, padx = 1, pady = 1)
-minus = Button(btns_frame, text = "-", fg = "black", width = 10, height = 3, bd = 0, bg = "#eee", cursor = "hand2", command = lambda: btn_click("-")).grid(row = 2, column = 3, padx = 1, pady = 1)
+minus = Button(btns_frame, text = "-", command = lambda: btn_click("-")).grid(row = 2, column = 3, padx = 1, pady = 1)
 
 
 # fourth row
 one   = Button(btns_frame, text = "1", command = lambda: btn_click(1)).grid(row = 3, column = 0, padx = 1, pady = 1)
 two   = Button(btns_frame, text = "2", command = lambda: btn_click(2)).grid(row = 3, column = 1, padx = 1, pady = 1)
 three = Button(btns_frame, text = "3", command = lambda: btn_click(3)).grid(row = 3, column = 2, padx = 1, pady = 1)
-plus  = Button(btns_frame, text = "+", fg = "black", width = 10, height = 3, bd = 0, bg = "#eee", cursor = "hand2", command = lambda: btn_click("+")).grid(row = 3, column = 3, padx = 1, pady = 1)
+plus  = Button(btns_frame, text = "+", command = lambda: btn_click("+")).grid(row = 3, column = 3, padx = 1, pady = 1)
 
 
 # fourth row
 zero   = Button(btns_frame, text = "0", command = lambda: btn_click(0)).grid(row = 4, column = 0, columnspan = 2, padx = 1, pady = 1)
-point  = Button(btns_frame, text = ".", fg = "black", width = 10, height = 3, bd = 0, bg = "#eee", cursor = "hand2", command = lambda: btn_click(".")).grid(row = 4, column = 2, padx = 1, pady = 1)
-equals = Button(btns_frame, text = "=", fg = "black", width = 10, height = 3, bd = 0, bg = "#eee", cursor = "hand2", command = lambda: btn_equal()).grid(row = 4, column = 3, padx = 1, pady = 1)
+point  = Button(btns_frame, text = ".", command = lambda: btn_click(".")).grid(row = 4, column = 2, padx = 1, pady = 1)
+equals = Button(btns_frame, text = "=", command = lambda: btn_equal()).grid(row = 4, column = 3, padx = 1, pady = 1)
 
 
 # tailor the NUMPAD buttons
@@ -102,5 +110,7 @@ for btn in btns_frame.winfo_children():
     if btn_text in NUMPAD:
         btn.configure(fg=FG_CLR_BTN, bg=BG_CLR_BTN, width=WIDTH_BTN, height=HEIGHT_BTN, bd=BD_BTN, cursor=CURSOR_BTN)
         if btn_text == "0": btn.configure(width=21)
-
+    else:
+        btn.configure(fg=FG_CLR_BTN, bg=BG_CLR_BTN_OPER, width=WIDTH_BTN, height=HEIGHT_BTN, bd=BD_BTN, cursor=CURSOR_BTN)
+        if btn_text == "C": btn.configure(width=32)
 window.mainloop()
